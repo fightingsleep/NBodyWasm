b0c2c5da18db2b8c74917fbe69aba9d32171b6d1
Chris Ross <chrislandonross@gmail.com>
2024-01-14 17:15:46 +0000

    Create GitHub pages branch

--- src/simulation/simulation.ts
+++
@@ -1,4 +0,0 @@
-export default abstract class Simulation {
-    abstract get_positions(): { position_x: number; position_y: number; position_z: number; mass: number; }[];
-    abstract simulate(dt: number): void;
-}
